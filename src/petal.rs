2a64588ae6769a5fc0c4f53be9ac517f67e878d1
unwx <unwx.jv@gmail.com>
2024-09-04 20:36:40 +0300

    feat: centralize tan petal generation to align with 0 degrees

--- src/petal.rs
+++ src/petal.rs
@@ -2,85 +2,74 @@ use crate::math::{to_cartesian, Rotate};
 use glam::Vec2;
 use std::f32::consts::PI;
 
-pub fn petal_sin(k: f32, step: f32) -> Vec<Vec2> {
+pub fn petal_sin(k: f32, step: f32, flip: bool) -> Vec<Vec2> {
     let theta_bound = 2.0 * (PI / (2.0 * k));
+    let mut petal = Vec::with_capacity((theta_bound / step) as usize + 1);
 
-    let mut max_point = Vec2::ZERO;
-    let mut max_length = 0.0;
+    eval_polar_func(k, step, 1.0, petal.capacity(), false, flip, f32::sin, &mut petal);
+    petal
+}
 
-    let mut petal: Vec<Vec2> = (0..=(theta_bound / step) as usize)
-        .map(|i| {
-            let theta = i as f32 * step;
-            let radius = (theta * k).sin();
+pub fn petal_tan(k1: f32, k2: f32, step: f32, flip: bool) -> Vec<Vec2> {
+    let eval_bound = |k: f32| -> f32 { PI / (4.0 * k) };
+    let eval_capacity = |bound: f32| -> usize { (bound / step) as usize + 1 };
 
-            let point = to_cartesian(radius, theta);
-            let length = point.length_squared();
+    let capacity1 = eval_capacity(eval_bound(k1));
+    let capacity2 = eval_capacity(eval_bound(k2));
+    let total_capacity = capacity1 + capacity2;
 
-            if max_length < length {
-                max_length = length;
-                max_point = point;
-            }
+    let mut petal = Vec::with_capacity(total_capacity);
+    eval_polar_func(k1, step, 0.5, capacity1, false, flip, f32::tan, &mut petal);
+    eval_polar_func(k2, step, 0.5, capacity2, true, flip, f32::tan, &mut petal);
 
-            point
-        })
-        .collect();
-
-    let is_petal_complex = k < 1.0;
-    let shift = if is_petal_complex { max_point.angle_to(Vec2::Y) } else { PI + max_point.angle_to(Vec2::Y) };
-
-    for i in 0..petal.len() {
-        petal[i] = petal[i].rotate_radians(shift);
-    }
-
-    if is_petal_complex {
-        for i in 0..petal.len() {
-            petal[i] = petal[i] - Vec2::Y;
-        }
+    for i in capacity1..total_capacity {
+        petal[i].x = -petal[i].x;
     }
 
     petal
 }
 
-pub fn petal_tan(k1: f32, k2: f32, step: f32) -> Vec<Vec2> {
-    let eval_bound = |k: f32| -> f32 {
-        PI / (4.0 * k)
-    };
-    let eval_capacity = |bound: f32| -> usize {
-        (bound / step) as usize + 1
-    };
+fn eval_polar_func<F>(
+    k: f32,
+    step: f32,
+    simple_func_k: f32,
+    size: usize,
+    reverse_values_order: bool,
+    flip_values: bool,
+    trig_func: F,
+    destination: &mut Vec<Vec2>,
+) where
+    F: Fn(f32) -> f32,
+{
+    let mut max_point = Vec2::ZERO;
+    let mut max_length = 0.0;
 
-    let theta_bound1 = eval_bound(k1);
-    let theta_bound2 = eval_bound(k2);
+    let start_index = destination.len();
 
-    let capacity1 = eval_capacity(theta_bound1);
-    let capacity2 = eval_capacity(theta_bound2);
-    let total_capacity = capacity1 + capacity2;
+    for i in 0..size {
+        let reversed_i = if reverse_values_order { size - i - 1 } else { i };
+        let theta = reversed_i as f32 * step;
+        let radius = trig_func(theta * k);
 
-    let mut points = Vec::with_capacity(total_capacity);
-    let mut eval_polar_func = |k: f32, times: usize| {
-        for i in 0..times {
-            let theta = i as f32 * step;
-            let radius = (theta * k).tan();
-            points.push(to_cartesian(radius, theta));
+        let point = to_cartesian(radius, theta);
+        if max_length < radius {
+            max_length = radius;
+            max_point = point;
         }
-    };
 
-    eval_polar_func(k1, capacity1);
-    eval_polar_func(k2, capacity2);
-
-    for i in capacity1..total_capacity {
-        points[i].x = -points[i].x;
+        destination.push(point);
     }
 
-    let angle_diff = {
-        let angle1 = points[capacity1 - 1].angle_to(Vec2::X);
-        let angle2 = points[total_capacity - 1].angle_to(Vec2::X);
-        angle2 - angle1
-    };
+    let should_flip = k < simple_func_k || flip_values;
+    let shift = if should_flip { max_point.angle_to(Vec2::Y) } else { PI + max_point.angle_to(Vec2::Y) };
 
-    for i in capacity1..total_capacity {
-        points[i] = points[i].rotate_radians(angle_diff);
+    for i in start_index..destination.len() {
+        destination[i] = destination[i].rotate_radians(shift);
     }
 
-    points
+    if should_flip {
+        for i in start_index..destination.len() {
+            destination[i] = destination[i] - Vec2::Y;
+        }
+    }
 }
