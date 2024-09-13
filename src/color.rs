8faed71ee5c66491318e8829c7eeb3a48b286184
unwx <unwx.jv@gmail.com>
2024-09-13 21:32:46 +0300

    feat: add random gradient and noise generation
    Additionally, add method that uses them to generate RGB array

--- src/color.rs
+++ src/color.rs
@@ -1,8 +1,454 @@
-use colorgrad::Color;
+use crate::math::normalize_f64;
+use colorgrad::{BasisGradient, BlendMode, CatmullRomGradient, Color, Gradient, GradientBuilder, LinearGradient, SharpGradient};
+use dyn_clone::{clone_trait_object, DynClone};
+use glam::IVec2;
+use noise::core::worley::{distance_functions, ReturnType};
+use noise::utils::{NoiseMapBuilder, PlaneMapBuilder};
+use noise::{BasicMulti, Billow, Curve, Fbm, HybridMulti, MultiFractal, NoiseFn, Perlin, PerlinSurflet, RidgedMulti, RotatePoint, Seedable, Simplex, SuperSimplex, Terrace, Turbulence, Worley};
 use palette::color_theory::{Analogous, Complementary, SplitComplementary, Tetradic, Triadic};
 use palette::Hsl;
+use rand::distributions::uniform::SampleRange;
 use rand::Rng;
 use std::cmp::Ordering;
+
+/*
+ * Gradient
+ */
+
+pub struct DynGradient {
+    gradient: Box<dyn Gradient>,
+}
+
+impl DynGradient {
+    pub fn new<G: Gradient + 'static>(gradient: G) -> Self {
+        Self { gradient: Box::new(gradient) }
+    }
+}
+
+impl Gradient for DynGradient {
+    fn at(&self, t: f32) -> Color {
+        self.gradient.at(t)
+    }
+
+    fn repeat_at(&self, t: f32) -> Color {
+        self.gradient.repeat_at(t)
+    }
+
+    fn reflect_at(&self, t: f32) -> Color {
+        self.gradient.reflect_at(t)
+    }
+
+    fn domain(&self) -> (f32, f32) {
+        self.gradient.domain()
+    }
+
+    fn colors(&self, n: usize) -> Vec<Color> {
+        self.gradient.colors(n)
+    }
+
+    fn sharp(&self, segment: u16, smoothness: f32) -> SharpGradient {
+        self.gradient.sharp(segment, smoothness)
+    }
+}
+
+
+/*
+ * Noise
+ */
+
+pub const MAX_OCTAVES: usize = 32;
+
+
+pub trait CloneableNoise<T, const DIM: usize>: NoiseFn<T, DIM> + DynClone {}
+clone_trait_object!(CloneableNoise<f64, 3>);
+
+impl CloneableNoise<f64, 3> for Perlin {}
+impl CloneableNoise<f64, 3> for PerlinSurflet {}
+impl CloneableNoise<f64, 3> for Simplex {}
+impl CloneableNoise<f64, 3> for SuperSimplex {}
+impl CloneableNoise<f64, 3> for Worley {}
+
+impl CloneableNoise<f64, 3> for Fbm<DynNoise<f64, 3>> {}
+impl CloneableNoise<f64, 3> for Billow<DynNoise<f64, 3>> {}
+impl CloneableNoise<f64, 3> for BasicMulti<DynNoise<f64, 3>> {}
+impl CloneableNoise<f64, 3> for HybridMulti<DynNoise<f64, 3>> {}
+impl CloneableNoise<f64, 3> for RidgedMulti<DynNoise<f64, 3>> {}
+
+impl CloneableNoise<f64, 3> for noise::Abs<f64, DynNoise<f64, 3>, 3> {}
+impl CloneableNoise<f64, 3> for noise::Negate<f64, DynNoise<f64, 3>, 3> {}
+impl CloneableNoise<f64, 3> for RotatePoint<DynNoise<f64, 3>> {}
+impl CloneableNoise<f64, 3> for Turbulence<DynNoise<f64, 3>, DynNoise<f64, 3>> {}
+impl CloneableNoise<f64, 3> for Curve<f64, DynNoise<f64, 3>, 3> {}
+impl CloneableNoise<f64, 3> for Terrace<f64, DynNoise<f64, 3>, 3> {}
+
+impl CloneableNoise<f64, 3> for noise::Add<f64, DynNoise<f64, 3>, DynNoise<f64, 3>, 3> {}
+impl CloneableNoise<f64, 3> for noise::Multiply<f64, DynNoise<f64, 3>, DynNoise<f64, 3>, 3> {}
+impl CloneableNoise<f64, 3> for noise::Power<f64, DynNoise<f64, 3>, DynNoise<f64, 3>, 3> {}
+impl CloneableNoise<f64, 3> for noise::Min<f64, DynNoise<f64, 3>, DynNoise<f64, 3>, 3> {}
+impl CloneableNoise<f64, 3> for noise::Max<f64, DynNoise<f64, 3>, DynNoise<f64, 3>, 3> {}
+
+
+pub struct DynNoise<T, const DIM: usize> {
+    noise: Box<dyn CloneableNoise<T, DIM>>,
+}
+
+impl<T, const DIM: usize> DynNoise<T, DIM> {
+    pub fn new<N: CloneableNoise<T, DIM> + 'static>(noise: N) -> Self {
+        Self { noise: Box::new(noise) }
+    }
+}
+
+impl<const DIM: usize> NoiseFn<f64, DIM> for DynNoise<f64, DIM> {
+    fn get(&self, point: [f64; DIM]) -> f64 {
+        // Prevent noise::math::vectors::Vector3<T>::floor_to_isize -> unwrap() panic
+        let mut safe_point: [f64; DIM] = [0.0; DIM];
+
+        for i in 0..DIM {
+            safe_point[i] = point[i].clamp(i16::MIN as f64, i16::MAX as f64)
+        }
+
+        self.noise.get(safe_point).clamp(i16::MIN as f64, i16::MAX as f64)
+    }
+}
+
+impl Default for DynNoise<f64, 3> {
+    fn default() -> Self {
+        DynNoise::new(Perlin::default())
+    }
+}
+
+impl Clone for DynNoise<f64, 3> {
+    fn clone(&self) -> Self {
+        Self { noise: self.noise.clone() }
+    }
+}
+
+/// [Seedable] is not object-safe, but is also required for fractal noise.
+/// This is a small workaround.
+impl<T, const DIM: usize> Seedable for DynNoise<T, DIM> {
+    fn set_seed(self, _: u32) -> Self {
+        self
+    }
+
+    fn seed(&self) -> u32 {
+        0
+    }
+}
+
+
+/*
+ * Methods
+ */
+
+pub fn colorize<G, N>(
+    range: &[(i32, &[(i32, i32)])],
+    gradient: &G,
+    noise: &N,
+    noise_bound: f64,
+) -> Vec<(IVec2, [u8; 4])>
+where
+    G: Gradient,
+    N: NoiseFn<f64, 3>,
+{
+    if range.is_empty() {
+        return vec![];
+    }
+
+    let (min_y, max_y, min_x, max_x, total_elements) = {
+        let min_y = range.first().unwrap().0;
+        let max_y = range.last().unwrap().0;
+
+        let mut min_x = i32::MAX;
+        let mut max_x = i32::MIN;
+        let mut total_elements = 0;
+
+        for (_, x_range) in range {
+            let first = x_range.first();
+            if first.is_none() {
+                continue;
+            }
+
+            let first = first.unwrap();
+            let last = x_range.last().unwrap();
+
+            min_x = min_x.min(first.0);
+            max_x = max_x.max(last.1);
+            total_elements += x_range.iter().map(|r| r.1 - r.0).sum::<i32>() as usize;
+        }
+
+        (
+            min_y,
+            max_y,
+            min_x,
+            max_x,
+            total_elements
+        )
+    };
+    if min_x == i32::MAX || max_x == i32::MIN {
+        return vec![];
+    }
+
+    let mut points = Vec::with_capacity(total_elements);
+    let noise_map = PlaneMapBuilder::new(&noise)
+        .set_size((max_x - min_x) as usize, (max_y - min_y) as usize)
+        .set_x_bounds(-noise_bound.abs(), noise_bound.abs())
+        .set_y_bounds(-noise_bound.abs(), noise_bound.abs())
+        .build();
+
+    let (min_noise, max_noise) = {
+        let mut min = f64::MAX;
+        let mut max = f64::MIN;
+
+        for value in &noise_map {
+            let value = *value;
+            if min > value { min = value; }
+            if max < value { max = value; }
+        }
+
+        (min, max)
+    };
+
+    for (y, x_ranges) in range {
+        for (from_x, to_x) in *x_ranges {
+            for x in *from_x..*to_x {
+                let noise_value = noise_map.get_value((x - min_x) as usize, (y - min_y) as usize);
+                let gradient_value = gradient.at(
+                    normalize_f64(
+                        noise_value,
+                        min_noise,
+                        max_noise,
+                        gradient.domain().0 as f64,
+                        gradient.domain().1 as f64,
+                    ) as f32
+                ).to_rgba8();
+
+                points.push((IVec2::new(x, *y), gradient_value))
+            }
+        }
+    }
+
+    points
+}
+
+pub fn random_gradient<R: Rng>(colors: &[Color], random: &mut R) -> DynGradient {
+    assert!(colors.len() > 1, "To build a gradient, you must provide at least two colors");
+    let blending_mode = match random.gen_range(0..3) {
+        0 => BlendMode::Rgb,
+        1 => BlendMode::LinearRgb,
+        2 => BlendMode::Oklab,
+        _ => unreachable!()
+    };
+
+    let mut builder = GradientBuilder::new();
+    let stages = builder.mode(blending_mode).colors(colors);
+
+    match random.gen_range(0..4) {
+        0 => stages.build::<LinearGradient>().map(DynGradient::new),
+        1 => stages.build::<BasisGradient>().map(DynGradient::new),
+        2 => stages.build::<CatmullRomGradient>().map(DynGradient::new),
+        3 => stages.build::<LinearGradient>()
+            .map(|g| {
+                g.sharp(
+                    random.gen_range(2..=32),
+                    random.gen_range(0.0..=1.0),
+                )
+            })
+            .map(DynGradient::new),
+        _ => unreachable!()
+    }.unwrap()
+}
+
+pub fn random_noise<R: Rng>(random: &mut R) -> DynNoise<f64, 3> {
+    fn decorate_noise<R: Rng>(noise: DynNoise<f64, 3>, decoration_chance: f32, random: &mut R) -> DynNoise<f64, 3> {
+        if random.gen_range(0.0..=1.0) > 1.0 - decoration_chance {
+            return noise;
+        }
+
+        match random.gen_range(0..6) {
+            0 => DynNoise::new(noise::Abs::new(noise)),
+            1 => DynNoise::new(noise::Negate::new(noise)),
+            2 => {
+                DynNoise::new(
+                    RotatePoint::new(noise)
+                        .set_angles(
+                            random.gen_range(-180.0..=180.0),
+                            random.gen_range(-180.0..=180.0),
+                            random.gen_range(-180.0..=180.0),
+                            random.gen_range(-180.0..=180.0),
+                        )
+                )
+            }
+            3 => {
+                DynNoise::new(
+                    Turbulence::new(noise)
+                        .set_frequency(random.gen_range(0.01..=10.0))
+                        .set_power(random.gen_range(0.1..=10.0))
+                        .set_roughness(random.gen_range(2..6))
+                )
+            }
+            4 => {
+                let mut curve = Curve::new(noise);
+                for point in rand_unique_f64_values(random.gen_range(4..=32), random, || -3.0..=3.0) {
+                    curve = curve.add_control_point(point, random.gen_range(-3.0..=3.0));
+                }
+
+                DynNoise::new(curve)
+            }
+            5 => {
+                let mut terrace = Terrace::new(noise).invert_terraces(random.gen_bool(0.5));
+                for point in rand_unique_f64_values(random.gen_range(2..=32), random, || -3.0..=3.0) {
+                    terrace = terrace.add_control_point(point);
+                }
+
+                DynNoise::new(terrace)
+            }
+            _ => unreachable!()
+        }
+    }
+
+    fn gen_noise<R: Rng>(seed: u32, fractal_chance: &mut f32, fractal_chance_acc: f32, decoration_chance: f32, random: &mut R) -> DynNoise<f64, 3> {
+        fn gen_noises<R: Rng>(size: usize, seed: u32, fractal_chance: &mut f32, fractal_chance_acc: f32, decoration_chance: f32, random: &mut R) -> Vec<DynNoise<f64, 3>> {
+            let mut noises = Vec::with_capacity(size);
+            for _ in 0..size {
+                *fractal_chance = (*fractal_chance + fractal_chance_acc).clamp(0.0, 1.0);
+                noises.push(gen_noise(seed, fractal_chance, fractal_chance_acc, decoration_chance, random));
+            }
+
+            noises
+        }
+
+        let noise = {
+            if random.gen_range(0.0..=1.0) > 1.0 - *fractal_chance {
+                let octaves = random.gen_range(2..(MAX_OCTAVES / 2));
+                let sources = gen_noises(octaves, seed, fractal_chance, fractal_chance_acc, decoration_chance, random);
+
+                match random.gen_range(0..5) {
+                    0 => {
+                        DynNoise::new(
+                            Fbm::new(seed)
+                                .set_octaves(octaves)
+                                .set_frequency(random.gen_range(0.01..=10.0))
+                                .set_lacunarity(random.gen_range(1.0..=3.0))
+                                .set_persistence(random.gen_range(0.2..=0.8))
+                                .set_sources(sources)
+                        )
+                    }
+                    1 => {
+                        DynNoise::new(
+                            Billow::new(seed)
+                                .set_octaves(octaves)
+                                .set_frequency(random.gen_range(0.01..=10.0))
+                                .set_lacunarity(random.gen_range(1.0..=3.0))
+                                .set_persistence(random.gen_range(0.2..=0.8))
+                                .set_sources(sources)
+                        )
+                    }
+                    2 => {
+                        DynNoise::new(
+                            BasicMulti::new(seed)
+                                .set_octaves(octaves)
+                                .set_frequency(random.gen_range(0.01..=10.0))
+                                .set_lacunarity(random.gen_range(1.0..=3.0))
+                                .set_persistence(random.gen_range(0.2..=0.8))
+                                .set_sources(sources)
+                        )
+                    }
+                    3 => {
+                        DynNoise::new(
+                            HybridMulti::new(seed)
+                                .set_octaves(octaves)
+                                .set_frequency(random.gen_range(0.01..=10.0))
+                                .set_lacunarity(random.gen_range(1.0..=3.0))
+                                .set_persistence(random.gen_range(0.2..=0.8))
+                                .set_sources(sources)
+                        )
+                    }
+                    4 => {
+                        DynNoise::new(
+                            RidgedMulti::new(seed)
+                                .set_octaves(octaves)
+                                .set_frequency(random.gen_range(0.01..=10.0))
+                                .set_lacunarity(random.gen_range(1.0..=3.0))
+                                .set_persistence(random.gen_range(0.2..=0.8))
+                                .set_attenuation(random.gen_range(0.1..=10.0))
+                                .set_sources(sources)
+                        )
+                    }
+                    _ => unreachable!()
+                }
+            } else {
+                match random.gen_range(0..5) {
+                    0 => DynNoise::new(Perlin::new(seed)),
+                    1 => DynNoise::new(PerlinSurflet::new(seed)),
+                    2 => DynNoise::new(Simplex::new(seed)),
+                    3 => DynNoise::new(SuperSimplex::new(seed)),
+                    4 => {
+                        DynNoise::new(
+                            Worley::new(seed)
+                                .set_frequency(random.gen_range(0.01..10.0))
+                                .set_return_type(match random.gen_bool(0.5) {
+                                    true => ReturnType::Value,
+                                    false => ReturnType::Distance
+                                })
+                                .set_distance_function(match random.gen_range(0..4) {
+                                    0 => distance_functions::euclidean,
+                                    1 => distance_functions::euclidean_squared,
+                                    2 => distance_functions::manhattan,
+                                    3 => distance_functions::chebyshev,
+                                    _ => unreachable!()
+                                })
+                        )
+                    }
+                    _ => unreachable!()
+                }
+            }
+        };
+
+        decorate_noise(noise, decoration_chance, random)
+    }
+
+    fn merge_noises<R: Rng>(mut noises: Vec<DynNoise<f64, 3>>, random: &mut R) -> DynNoise<f64, 3> {
+        if noises.is_empty() {
+            return DynNoise::default();
+        }
+        if noises.len() == 1 {
+            return noises[0].clone();
+        }
+
+        while noises.len() > 1 {
+            let len = noises.len();
+            let noise1 = noises[len - 2].clone();
+            let noise2 = noises[len - 1].clone();
+
+            let merged = match random.gen_range(0..4) {
+                0 => DynNoise::new(noise::Add::new(noise1, noise2)),
+                1 => DynNoise::new(noise::Multiply::new(noise1, noise2)),
+                2 => DynNoise::new(noise::Min::new(noise1, noise2)),
+                3 => DynNoise::new(noise::Max::new(noise1, noise2)),
+                _ => unreachable!()
+            };
+
+            noises.remove(len - 1);
+            noises[len - 2] = merged;
+        }
+
+        noises[0].clone()
+    }
+
+    let seed = random.next_u32();
+    let decoration_chance = random.gen_range(0.0..=1.0);
+
+    let mut fractal_chance = random.gen_range(0.0..=1.0);
+    let fractal_chance_acc = random.gen_range(-0.5..-0.05);
+    let fractal_chance_ref = &mut fractal_chance;
+
+    let mut noises = Vec::with_capacity(random.gen_range(1..16));
+    for _ in 0..noises.capacity() {
+        noises.push(gen_noise(seed, fractal_chance_ref, fractal_chance_acc, decoration_chance, random));
+    }
+
+    merge_noises(noises, random)
+}
 
 pub fn random_palette<R>(
     size: usize,
@@ -11,6 +457,7 @@ pub fn random_palette<R>(
 ) -> Vec<Color> where
     R: Rng,
 {
+    assert!(size <= 12, "palette can only contain up to 12 colors, size: {size}");
     if size <= 1 {
         return vec![Color::from_hsla(f32::from(primary_color.hue), primary_color.saturation, primary_color.lightness, 1.0)];
     }
@@ -20,7 +467,7 @@ pub fn random_palette<R>(
 
     while palette.len() < size {
         let base_color = palette[random.gen_range(0..palette.len())];
-        let mut split = match random.gen_range(0..=4) {
+        let split = match random.gen_range(0..=4) {
             0 => vec![base_color.complementary()],
             1 => {
                 let c = base_color.split_complementary();
@@ -38,7 +485,7 @@ pub fn random_palette<R>(
                 let c = base_color.tetradic();
                 vec![c.0, c.1, c.2]
             }
-            _ => panic!("Invalid random range in random_palette() method")
+            _ => unreachable!()
         };
 
         for i in 0..split.len().min(size - palette.len()) {
@@ -62,4 +509,29 @@ pub fn random_palette<R>(
         .iter()
         .map(|hsl| Color::from_hsla(f32::from(hsl.hue), hsl.saturation, hsl.lightness, 1.0))
         .collect()
+}
+
+/// O(n) warning, small `size` expected.
+fn rand_unique_f64_values<Rand, Range, RangeFn>(size: usize, random: &mut Rand, range: RangeFn) -> Vec<f64>
+where
+    Rand: Rng,
+    Range: SampleRange<f64>,
+    RangeFn: Fn() -> Range,
+{
+    let mut values: Vec<f64> = Vec::with_capacity(size);
+    let mut failures = 0;
+
+    while values.len() < size {
+        let value: f64 = random.gen_range(range());
+        if !values.iter().any(|x| (x - value).abs() < f64::EPSILON) {
+            values.push(value);
+        } else {
+            failures += 1;
+            if failures > size * 100 {
+                panic!("infinite loop detected in rand_unique_f64_values() method")
+            }
+        }
+    }
+
+    values
 }
--- src/math.rs
+++ src/math.rs
@@ -7,6 +7,10 @@ pub fn to_cartesian(
     Vec2::new(radius * theta.cos(), radius * theta.sin())
 }
 
+pub fn normalize_f64(value: f64, old_min: f64, old_max: f64, new_min: f64, new_max: f64) -> f64 {
+    new_min + (value - old_min) * (new_max - new_min) / (old_max - old_min)
+}
+
 
 pub trait Rotate: Copy + Clone {
     fn rotate_radians(self, angle: f32) -> Self;
